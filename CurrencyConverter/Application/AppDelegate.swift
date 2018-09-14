dbdac9fb181ac60c9e8a9b271c4482f000e7cd13
Guowei Mo <guowei.mo13@gmail.com>
2018-09-14 21:51:30 +0100

    created project structure.

---
+++ CurrencyConverter/Application/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  CurrencyConverter
+//
+//  Created by Guowei Mo on 14/09/2018.
+//  Copyright © 2018 Guowei Mo. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+}
+
